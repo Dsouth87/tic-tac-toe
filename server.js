02cadf9cd364a7206ef7b97d9b3dbde20e05cabe
derek southard <derek.southard@airbnb.com>
2020-07-06 10:09:45 -0700

    added express server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+const PORT = process.env.PORT || 8800
+
+app.use(express.static('public'))
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+})
